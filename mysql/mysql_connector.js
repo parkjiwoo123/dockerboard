ced54486c7b70f10b229b3cab7a718470e4cae9a
parkjiwoo <h591264@naver.com>
2017-07-27 10:38:32 +0900

    modify rootpassword

--- mysql/mysql_connector.js
+++ mysql/mysql_connector.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const client = mysql.createConnection({
   host: "54.82.110.167:3306",
   user: "root",
-  password: "1234",
+  password: "root",
   database: "board"
 });
 
